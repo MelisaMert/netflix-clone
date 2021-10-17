d5bc31675c50904bde05634d7cdac8097a042046
MelisaMert <melisamert024@gmail.com>
2021-10-18 00:15:57 +0300

    client app created and coding

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Home from './pages/home/Home';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+// import Watch from "./pages/watch/Watch.jsx";
+import './app.scss';
+
+const App = () => {
+  return (
+    <div>
+      {/* <Register /> */}
+      <Login />
+      {/* <Register /> */}
+        {/* <Home />  */}
+       {/* <Watch />  */}
+    </div>
+  )
+}
+
+export default App
